6f54abd745e856cbe8ea32f9b5d4d744ea445b40
Jérôme Mahuet <jerome.mahuet@gmail.com>
2014-06-28 12:21:28 +0200

    tests

---
+++ src/test/scala/com/rydgel/yo/YoClientSuite.scala
@@ -0,0 +1,27 @@
+package com.rydgel.yo
+
+import scala.concurrent.duration._
+import scala.concurrent.Await
+import org.scalatest._
+import scala.io.Source
+
+class YoClientSuite extends FunSuite {
+  val key = new ApiToken(
+    Source.fromURL(getClass.getResource("/token.txt")).getLines().mkString
+  )
+
+  test("YoAll request with a good API key should be ok") {
+    println(key)
+    val request = YoClient.yoAll(key)
+    val result = Await.result(request, 5 seconds)
+    assert(result.isInstanceOf[YoResponse])
+  }
+
+  test("YoAll request with a bad API key should be an error") {
+    val request = YoClient.yoAll(ApiToken("bad-key"))
+
+    intercept[YoClientException] {
+      Await.result(request, 5 seconds)
+    }
+  }
+}
